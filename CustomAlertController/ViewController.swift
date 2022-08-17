a96ac14d84d98addf23ce471c9c0844ae62e92a6
Piesoft <nut.jirapornpraneet@gmail.com>
2022-08-17 15:55:26 +0700

    add work schedule in view controller

--- CustomAlertController/ViewController.swift
+++ CustomAlertController/ViewController.swift
@@ -11,9 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
+    
+    @IBAction func alertTapped() {
+        let utils = Utils()
+        
+        let button1 = AlertButton(title: "Yes", action: {
+            print("Yes clicked")
+        }, titleColor: UIColor.red, backgroundColor: UIColor.clear)
+        
+        let button2 = AlertButton(title: "No", action: {
+            print("No clicked")
+        }, titleColor: UIColor.lightGray, backgroundColor: UIColor.clear)
+        
+        let alertPayload = AlertPayload(title: "Two Button Alert", titleColor: UIColor.red, message: "Are you sure you want to delete?", messageColor: UIColor.green, buttons: [button1, button2], backgroundColor: UIColor.yellow)
+        
+        utils.showAlert(payload: alertPayload, parentViewController: self)
+    }
 }
 
