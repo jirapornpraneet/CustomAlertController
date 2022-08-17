525609cb98530756e6eba1d5e689b72b773861f4
Piesoft <nut.jirapornpraneet@gmail.com>
2022-08-17 15:49:20 +0700

    edit code

--- CustomAlertController/Utils.swift
+++ CustomAlertController/Utils.swift
@@ -10,26 +10,15 @@ import UIKit
 class Utils {
     
     func showAlert(payload: AlertPayload, parentViewController: UIViewController) {
-        var customAlertController: CustomAlertController!;
-        if (payload.buttons.count == 1) {
-            customAlertController = self.instantiateViewController(storyboardName: "Alerts", viewControllerIdentifier: "OneButtonAlert") as! CustomAlertController;
-        }
-        else if (payload.buttons.count == 2) {
-            customAlertController = self.instantiateViewController(storyboardName: "Alerts", viewControllerIdentifier: "TwoButtonAlert") as! CustomAlertController;
-        }
-        else {
-            // Action not supported
-            return;
-        }
-        customAlertController?.payload = payload;
-        
-        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert);
-        alertController.setValue(customAlertController, forKey: "contentViewController");
-        parentViewController.present(alertController, animated: true, completion: nil);
+        let customAlertController = self.instantiateViewController(storyboardName: "Alerts", viewControllerIdentifier: "CustomAlertController") as! CustomAlertController
+        customAlertController.payload = payload
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        alertController.setValue(customAlertController, forKey: "contentViewController")
+        parentViewController.present(alertController, animated: true, completion: nil)
     }
     
     public func instantiateViewController(storyboardName: String, viewControllerIdentifier: String) -> UIViewController {
-        let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle.main);
-        return storyboard.instantiateViewController(withIdentifier: viewControllerIdentifier);
+        let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: viewControllerIdentifier)
     }
 }
