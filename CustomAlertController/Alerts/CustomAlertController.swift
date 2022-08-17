6caee8f24de862230bfaecfb080bf3c23392763c
Piesoft <nut.jirapornpraneet@gmail.com>
2022-08-17 15:41:00 +0700

    add class CustomAlertController

---
+++ CustomAlertController/Alerts/CustomAlertController.swift
@@ -0,0 +1,29 @@
+//
+//  CustomAlertController.swift
+//  CustomAlertController
+//
+//  Created by Piesoft on 17/8/2565 BE.
+//
+
+import UIKit
+
+class CustomAlertController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
